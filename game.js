73d0ca32f03d8eb4bd4969c00c52ff61141bdf6a
João Pedro <jplopes5555@gmail.com>
2024-06-27 21:55:44 +0100

    Starting logic

--- game.js
+++ game.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    DistributeCards();
+    let SelectedCardUp = 0;
+    let SelectedCardDown = 0;
+    $(".Up-5-Cards button").on("click", function () {
+        SelectedCardUp = $(this).attr("class");
+        console.log(SelectedCardUp);
+    })
+
+    $(".Down-5-Cards button").on("click", function () {
+        SelectedCardDown = $(this).attr("class");
+        console.log(SelectedCardDown);
+    })
+
     
 })
+
+function DistributeCards() {
+    index = 0
+    $(".Up-5-Cards button").each(function () {
+        index++;
+        $(this).text(index);
+    });
+
+    $(".Down-5-Cards button").each(function () {
+        index++;
+        $(this).text(index);
+    });
+
+}
+
+
+
